542e75de2e2f4d3cce9bcfaa840b463808bbb517
Leonardo Cserny <leo.cserny@gmail.com>
2025-11-27 10:48:13 +0200

    add participant generation

--- src/app/generate/generate.component.ts
+++ src/app/generate/generate.component.ts
@@ -1,55 +1,86 @@
 import {Component, OnInit} from '@angular/core';
 import {NamesWrapper, NameTokenPair} from '../models/matches.model';
-import {Router} from '@angular/router';
 import {NgForOf, NgIf} from '@angular/common';
 import {MatchesService} from '../matches.service';
-import {FormsModule} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-generate',
   imports: [
-    NgIf,
     NgForOf,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
   ],
-  templateUrl: './generate.component.html'
+  templateUrl: './generate.component.html',
+  styleUrls: ['./generate.component.scss']
 })
-export class GenerateComponent implements OnInit {
+export class GenerateComponent {
 
-  namesInput: string = '';
+  error: string = "";
   links: string[] = [];
+  form: FormGroup;
 
-  constructor(private matchesService: MatchesService, private router: Router) {
+  constructor(private matchesService: MatchesService, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      participant: this.fb.array([this.createParticipant()])
+    });
   }
 
-  ngOnInit(): void {
+  get participants(): FormArray {
+    return this.form.get('participant') as FormArray;
   }
 
-  processJson() {
-    try {
-      /*
-      {
-        "names": {
-          "leo": ["sabi", "casi"],
-          "sabi": ["leo", "casi"],
-          "casi": ["sabi", "leo"],
-          "alis": ["matei", "ana"],
-          "matei": ["alis", "ana"],
-          "ana": ["matei", "alis"]
-        }
-      }
-       */
-      const namesWrapper: NamesWrapper = JSON.parse(this.namesInput);
-      this.matchesService.generateLinks(namesWrapper).subscribe(links => {
+  createParticipant(): FormGroup {
+    return this.fb.group({
+      name: [''],
+      exclusions: this.fb.array([this.createExclusion()])
+    });
+  }
+
+  addParticipant() {
+    this.participants.push(this.createParticipant());
+  }
+
+  createExclusion(): FormGroup {
+    return this.fb.group({
+      exclusion: ['']
+    });
+  }
+
+  getExclusions(idx: number): FormArray {
+    return this.participants.at(idx).get('exclusions') as FormArray;
+  }
+
+  addExclusio(idx: number) {
+    this.getExclusions(idx).push(this.createExclusion());
+  }
+
+  // TODO
+  parseParticipants() {
+    const data: {
+      name: string,
+      exclusions: string[]
+    }[] = this.participants.value.map((participant: any, idx: number) => ({
+      name: participant.name,
+      exclusions: participant.exclusions.map((i: any) => i.exclusion).filter((v: string) => v.trim() !== '')
+    }));
+
+    const namesWrapper: NamesWrapper = {
+      names: data.reduce((acc, curr) => {
+        acc[curr.name] = curr.exclusions;
+        return acc;
+      }, {} as Record<string, string[]>),
+    };
+
+    this.matchesService.generateLinks(namesWrapper).subscribe({
+      next: links => {
         this.links = links;
-      })
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  clear() {
-    this.namesInput = '';
-    this.links = [];
+        this.error = "";
+      },
+      error: err => {
+        this.error = err.message || JSON.stringify(err);
+      }
+    });
   }
 }
